a20143bda8a934d0208bfcdf65286fb6a979ca7a
qam022 <mowmowmow296@gmail.com>
2021-02-27 15:24:50 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ function setup() {
   createCanvas(800,450);
   $.get("exampledata.json", 
         function(data){
-          for(var i=0;i++;i<data.length){
+          for(var i=0;i<data.length;i++){
             document.getElementById("datafromjson").innerHTML+=data[i].name+"<br>";
           }
         }
