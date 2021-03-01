a8a9cd212bb88dcb4b764c4df6361965158a2323
qam022 <mowmowmow296@gmail.com>
2021-03-01 19:13:05 +0000

    cool

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
 var w;
 var h;
 var t=0;
+var tracks = [];
+var frameRateDisplay = false;
 function setup() {
+  frameRate(60);
   w=windowWidth*0.75;
   h=windowHeight*0.95;
   createCanvas(w,h);
@@ -20,8 +23,11 @@ function setup() {
   for (var i=0;i<data.length;i++){
     //initialise positions
     data[i].xpos=w/2;
-    data[i].ypos=h/2-(data[i].semimajoraxis*200);
+    data[i].ypos=h/2-(i*50);
     //document.getElementById("datafromjson").innerHTML+=data[i].name+"<br>";
+    if (data[i].name != "Sun"){
+      
+    }
   }
 }
 
@@ -31,23 +37,45 @@ function draw() {
   t++;
   for (var i=0;i<data.length;i++){
     //update positions
-    data[i].xpos=w/2+data[i].semimajoraxis*200*Math.sin((t*PI/(180*data[i].orbitperiod))%TWO_PI);
-    data[i].ypos=h/2-data[i].semimajoraxis*200*Math.cos((t*PI/(180*data[i].orbitperiod))%TWO_PI);
+    data[i].xpos=w/2+i*50*Math.sin((t*0.62*PI/(180*data[i].orbitperiod))%TWO_PI);
+    data[i].ypos=h/2-i*50*Math.cos((t*0.62*PI/(180*data[i].orbitperiod))%TWO_PI);
     if (data[i].xpos != -1 && data[i].ypos != -1){ //makes sure my values are initialised
       fill(data[i].colour);
       noStroke();
       if(data[i].name != "Sun"){
-        circle(data[i].xpos,data[i].ypos,data[i].radius*15);
+        circle(data[i].xpos,data[i].ypos,data[i].radius*30);
+        temp = new Track(data[i].colour,data[i].xpos,data[i].ypos);
+        tracks.push(temp);
       }
       else{
         circle(data[i].xpos,data[i].ypos,data[i].radius*0.4);
       }
     }
+
     else{
       textSize(34);
       textAlign(LEFT, TOP);
       fill("#ff0000");
       text("error: hey something went wrong with initialising the position values please tell me about this",0,0);
+    }
+    
+    if (frameRateDisplay){
+      textSize(24);
+      textAlign(LEFT,TOP);
+      fill("#00ff00");
+      text(Math.round(frameRate()),0,0);
+    }
+    
+    //draw tracks
+    for(var j=0;j<tracks.length;j++){
+      fill(tracks[j].updateAndGetAlpha(tracks[j].colour,tracks[j].alpha));
+      noStroke();
+      circle(tracks[j].x,tracks[j].y,3);
+      tracks[j].lifetime--;
+      tracks[j].settrackAlpha = tracks[j].alpha - (255/tracks[0].maxLifetime);
+      if (tracks[j].lifetime <= 0){
+        tracks.splice(j,1);
+      }
     }
   }
 }
---
+++ track.js
@@ -0,0 +1,17 @@
+class Track{
+    constructor(colour,x,y){
+        this.colour = color(colour);
+        this.x = x;
+        this.y = y;
+        this.alpha = 255;
+        this.maxLifetime = 400;
+        this.lifetime = this.maxLifetime; //in frames 
+    }
+    set settrackAlpha(newalpha){
+        this.alpha = newalpha;
+    }
+    updateAndGetAlpha(colour,alpha){
+        colour.setAlpha(alpha);
+        return colour;
+    }
+}
